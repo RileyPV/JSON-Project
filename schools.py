4cf9ea77be671f5e66072272d9112d2fe2c0edcf
Riley Richards <riley_richards1@baylor.edu>
2022-03-16 14:39:28 -0500

    Changes to filter out schools that arent a power 5 conference

--- schools.py
+++ schools.py
@@ -23,18 +23,86 @@ univ_data = json.load(infile)
 
 json.dump(univ_data, outfile, indent = 4)
 
-power_five_schools = []
+power_five_schools = {}
+
+#inst_name = []
+
+women_grad_rate, aa_enroll, instate_price, hover_texts = [],[],[],[]
 
 for school in univ_data:
-    if univ_data["NAIA conference number football (IC2020)"] == 102 or 107 or 108 or 127 or 130:
-        power_five_schools.append(school)
+    if school["NCAA"]["NAIA conference number football (IC2020)"] == 102 or 107 or 108 or 127 or 130:
+        title = school["instnm"]
+        grad_rate = school["Graduation rate  women (DRVGR2020)"]
+        enroll = school["Percent of total enrollment that are Black or African American (DRVEF2020)"]
+        price = school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"]
+        women_grad_rate.append(grad_rate)
+        aa_enroll.append(enroll)
+        instate_price.append(price)
+        hover_texts.append(title)
         
-
-
-women_grad_rate, aa_enroll, instate_price = [],[],[]
-
+print(women_grad_rate[:10])
+print(aa_enroll[:10])
+print(instate_price[:10])
 
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
+#Map 1
+#data = [{'type' : 'Scattergeo',
+ #   'lon' : lons,
+  #  'lat' : lats,
+   # 'text' : hover_texts,
+    #'marker' : {
+     #   'size' : [5 * mags for mag in mags if mag > 3],
+      #  'color' : mags,
+       # 'colorscale' : 'Viridis',
+        #'reversescale' : True,
+        #'colorbar' : {'title' : 'Magnitude'}}
+    #},]
+    
 
+#my_layout = Layout(title='Global Earthquakes')
+
+#fig = {'data':data, 'layout':my_layout}
+
+#offline.plot(fig,filename='global_earthquakes.html')
+
+#Map 2
+#data = [{'type' : 'Scattergeo',
+ #   'lon' : lons,
+  #  'lat' : lats,
+   # 'text' : hover_texts,
+   # 'marker' : {
+   #     'size' : [5 * mags for mag in mags if mag > 3],
+    #    'color' : mags,
+     #   'colorscale' : 'Viridis',
+      #  'reversescale' : True,
+       # 'colorbar' : {'title' : 'Magnitude'}}
+    #},]
+    
+
+#my_layout = Layout(title='Global Earthquakes')
+
+#fig = {'data':data, 'layout':my_layout}
+
+#offline.plot(fig,filename='global_earthquakes.html')
+
+#Map 3
+#data = [{'type' : 'Scattergeo',
+ #   'lon' : lons,
+  #  'lat' : lats,
+   # 'text' : hover_texts,
+   # 'marker' : {
+    #    'size' : [5 * mags for mag in mags if mag > 3],
+     #   'color' : mags,
+      #  'colorscale' : 'Viridis',
+       # 'reversescale' : True,
+       # 'colorbar' : {'title' : 'Magnitude'}}
+   # },]
+    
+
+#my_layout = Layout(title='Global Earthquakes')
+
+#fig = {'data':data, 'layout':my_layout}
+
+#offline.plot(fig,filename='global_earthquakes.html')

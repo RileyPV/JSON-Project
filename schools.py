a8fcabddbb6429d0a5f21dcb70f1ec400f0df22b
Riley Richards <riley_richards1@baylor.edu>
2022-03-16 20:53:44 -0500

    appended map element to title with institution name

--- schools.py
+++ schools.py
@@ -15,94 +15,148 @@ Map 3) Total price for in-state students living off campus over $50,000
 
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 infile = open('univ.json', 'r')
 outfile = open('readable_univ_data.json', 'w')
-
 
 univ_data = json.load(infile)
 
 json.dump(univ_data, outfile, indent = 4)
 
-power_five_schools = {}
+women_grad_rate, aa_enroll, instate_price, hover_texts = [],[],[],[]
 
-#inst_name = []
+lons, lats, sizes = [],[],[]
+
+for school in univ_data:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in [102, 107,108, 127, 130]:
+        lon = school["Longitude location of institution (HD2020)"]
+        lat = school["Latitude location of institution (HD2020)"]
+        size = school["Total  enrollment (DRVEF2020)"]
+        grad_rate = school["Graduation rate  women (DRVGR2020)"]
+        if grad_rate > 50:
+            women_grad_rate.append(grad_rate)
+        title = [school["instnm"], grad_rate]
+        #enroll = school["Percent of total enrollment that are Black or African American (DRVEF2020)"]
+        #price = school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"]
+        lons.append(lon)
+        lats.append(lat)
+        sizes.append(size)
+        hover_texts.append(title)
+        #if enroll > 10:    
+           # aa_enroll.append(enroll)
+        #if price > 50000:
+            #instate_price.append(price)
+        
+        
+print(women_grad_rate[:10])
+#print(aa_enroll[:10])
+#print(instate_price[:10])
+print(hover_texts[:10])
+
+
+#Map 1
+data = [{'type' : 'scattergeo',
+    'lon' : lons,
+    'lat' : lats,
+    'text' : [hover_texts,grad_rate],
+    'marker' : {
+        'size' : [.0002 * size for size in sizes],
+        'color' : sizes,
+        'colorscale' : 'Viridis',
+        'reversescale' : True,
+        'colorbar' : {'title' :'Graduation Rate'}},
+    },]
+
+my_layout = Layout(title='Power 5 Conference Graduation rate for Women')
+
+fig = {'data':data, 'layout':my_layout}
+
+offline.plot(fig,filename='women_grad_rate.html')
 
 women_grad_rate, aa_enroll, instate_price, hover_texts = [],[],[],[]
 
+lons, lats, sizes = [],[],[]
+
 for school in univ_data:
-    if school["NCAA"]["NAIA conference number football (IC2020)"] == 102 or 107 or 108 or 127 or 130:
-        title = school["instnm"]
-        grad_rate = school["Graduation rate  women (DRVGR2020)"]
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in [102, 107,108, 127, 130]:
+        lon = school["Longitude location of institution (HD2020)"]
+        lat = school["Latitude location of institution (HD2020)"]
+        size = school["Total  enrollment (DRVEF2020)"]
         enroll = school["Percent of total enrollment that are Black or African American (DRVEF2020)"]
-        price = school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"]
-        women_grad_rate.append(grad_rate)
-        aa_enroll.append(enroll)
-        instate_price.append(price)
+        if enroll > 10:    
+            aa_enroll.append(enroll)
+        title = [school["instnm"], enroll]
+        lons.append(lon)
+        lats.append(lat)
+        sizes.append(size)
         hover_texts.append(title)
-        
-print(women_grad_rate[:10])
+
 print(aa_enroll[:10])
-print(instate_price[:10])
-
-from plotly.graph_objs import Scattergeo, Layout
-from plotly import offline
-
-#Map 1
-#data = [{'type' : 'Scattergeo',
- #   'lon' : lons,
-  #  'lat' : lats,
-   # 'text' : hover_texts,
-    #'marker' : {
-     #   'size' : [5 * mags for mag in mags if mag > 3],
-      #  'color' : mags,
-       # 'colorscale' : 'Viridis',
-        #'reversescale' : True,
-        #'colorbar' : {'title' : 'Magnitude'}}
-    #},]
-    
-
-#my_layout = Layout(title='Global Earthquakes')
-
-#fig = {'data':data, 'layout':my_layout}
-
-#offline.plot(fig,filename='global_earthquakes.html')
+print(hover_texts[:10])
 
 #Map 2
-#data = [{'type' : 'Scattergeo',
- #   'lon' : lons,
-  #  'lat' : lats,
-   # 'text' : hover_texts,
-   # 'marker' : {
-   #     'size' : [5 * mags for mag in mags if mag > 3],
-    #    'color' : mags,
-     #   'colorscale' : 'Viridis',
-      #  'reversescale' : True,
-       # 'colorbar' : {'title' : 'Magnitude'}}
-    #},]
+data = [{'type' : 'scattergeo',
+    'lon' : lons,
+    'lat' : lats,
+    'text' : hover_texts,
+    'marker' : {
+        'size' : [.0002 * size for size in sizes],
+        'color' : sizes,
+        'colorscale' : 'Viridis',
+        'reversescale' : True,
+        'colorbar' : {'title' : 'Enrollment Rate'}}
+    },]
     
 
-#my_layout = Layout(title='Global Earthquakes')
+my_layout = Layout(title='Power 5 Conference Enrollment Rate for African Americans')
 
-#fig = {'data':data, 'layout':my_layout}
+fig = {'data':data, 'layout':my_layout}
 
-#offline.plot(fig,filename='global_earthquakes.html')
+offline.plot(fig,filename='enrollment_rate.html')
+
+'''
+women_grad_rate, aa_enroll, instate_price, hover_texts = [],[],[],[]
+
+lons, lats, sizes = [],[],[]
+
+for school in univ_data:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in [102, 107,108, 127, 130]:
+        lon = school["Longitude location of institution (HD2020)"]
+        lat = school["Latitude location of institution (HD2020)"]
+        size = school["Total  enrollment (DRVEF2020)"]
+        price = school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"]
+        title = school["instnm"] & price
+        lons.append(lon)
+        lats.append(lat)
+        sizes.append(size)
+        hover_texts.append(title)
+        if price > 50000:
+            instate_price.append(price)
+
+print(price[:10])
+print(hover_texts[:10])
+
+
 
 #Map 3
-#data = [{'type' : 'Scattergeo',
- #   'lon' : lons,
-  #  'lat' : lats,
-   # 'text' : hover_texts,
-   # 'marker' : {
-    #    'size' : [5 * mags for mag in mags if mag > 3],
-     #   'color' : mags,
-      #  'colorscale' : 'Viridis',
-       # 'reversescale' : True,
-       # 'colorbar' : {'title' : 'Magnitude'}}
-   # },]
+data = [{'type' : 'scattergeo',
+    'lon' : lons,
+    'lat' : lats,
+    'text' : hover_texts,
+    'marker' : {
+        'size' : [.0002 * size for size in sizes],
+        'color' : sizes,
+        'colorscale' : 'Viridis',
+        'reversescale' : True,
+        'colorbar' : {'title' : 'Total In-state Price'}}
+    },]
     
 
-#my_layout = Layout(title='Global Earthquakes')
+my_layout = Layout(title='Power 5 Conference In-state Price For Students Living Off Campus')
 
-#fig = {'data':data, 'layout':my_layout}
+fig = {'data':data, 'layout':my_layout}
 
-#offline.plot(fig,filename='global_earthquakes.html')
+offline.plot(fig,filename='total_price.html')
+'''
